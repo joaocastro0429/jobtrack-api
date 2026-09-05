eb8a556afd6a343ee00848227abd085a0d2d60f9
João Castro <joaocastro12629@gmail.com>
2026-09-05 20:34:44 -0300

    feat: add Company create endpoint

--- src/main/java/br/com/joaocastro/jobtrack/controller/CompanyController.java
+++ src/main/java/br/com/joaocastro/jobtrack/controller/CompanyController.java
@@ -1,4 +1,28 @@
 package br.com.joaocastro.jobtrack.controller;
 
+import br.com.joaocastro.jobtrack.dto.CompanyRequest;
+import br.com.joaocastro.jobtrack.entity.Company;
+import br.com.joaocastro.jobtrack.service.CompanyService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/companies")
 public class CompanyController {
+    final private CompanyService companyService;
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+     @PostMapping
+     @ResponseStatus(HttpStatus.CREATED)
+
+     public Company create(@Valid @RequestBody CompanyRequest  request) {
+         Company company = new Company();
+         company.setName(request.name());
+         company.setWebsite(request.website());
+
+         return companyService.create(company);
+
+    }
 }
--- src/main/java/br/com/joaocastro/jobtrack/dto/CompanyRequest.java
+++ src/main/java/br/com/joaocastro/jobtrack/dto/CompanyRequest.java
@@ -1,4 +1,16 @@
 package br.com.joaocastro.jobtrack.dto;
 
-public class CompanyRequest {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CompanyRequest(
+
+        @NotBlank(message = "Company name is required")
+        @Size(max = 150, message = "Company name must have at most 150 characters")
+        String name,
+
+        @Size(max = 255, message = "Website must have at most 255 characters")
+        String website
+
+) {
 }
