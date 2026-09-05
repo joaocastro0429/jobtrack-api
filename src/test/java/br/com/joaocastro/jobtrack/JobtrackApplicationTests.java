7f7c4fd719684b5700f2e2760fc5a5fbbc3c68df
João Castro <joaocastro12629@gmail.com>
2026-09-05 11:10:48 -0300

    projeto  jobtrack

---
+++ src/test/java/br/com/joaocastro/jobtrack/JobtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaocastro.jobtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
