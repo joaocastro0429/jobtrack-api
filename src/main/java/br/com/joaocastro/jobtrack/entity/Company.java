07dbe1ec7b306165c2d3253e07e9001492bb9520
João Castro <joaocastro12629@gmail.com>
2026-09-05 17:49:40 -0300

    feat: add Company entity

--- src/main/java/br/com/joaocastro/jobtrack/entity/Company.java
+++ src/main/java/br/com/joaocastro/jobtrack/entity/Company.java
@@ -1,4 +1,31 @@
 package br.com.joaocastro.jobtrack.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(length = 255)
+    private String website;
 }
